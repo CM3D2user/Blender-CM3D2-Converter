06108e49716a52280a2ebb55741223be907de41d
CM3Duser <cm3duser@gmail.com>
2015-08-14 22:58:56 +0900

    ボーン情報をテキストとして読み込むように

--- CM3D2 Converter/model_import.py
+++ CM3D2 Converter/model_import.py
@@ -12,9 +12,16 @@ class import_cm3d2_model(bpy.types.Operator):
 	bl_idname = "import_mesh.import_cm3d2_model"
 	bl_label = "CM3D2 Model (.model)"
 	bl_description = "カスタムメイド3D2のmodelファイルを読み込みます"
-	bl_options = {'REGISTER', 'UNDO'}
+	bl_options = {'REGISTER'}
 	
 	filepath = bpy.props.StringProperty(subtype='FILE_PATH')
+	filename_ext = ".model"
+	filter_glob = bpy.props.StringProperty(default="*.model", options={'HIDDEN'})
+	
+	is_mesh = bpy.props.BoolProperty(name="メッシュデータの読み込み", default=True)
+	is_remove_doubles = bpy.props.BoolProperty(name="重複頂点を結合", default=True)
+	is_bone_data = bpy.props.BoolProperty(name="ボーン情報のテキスト読み込み", default=True)
+	is_local_bone_data = bpy.props.BoolProperty(name="ローカルボーン情報のテキスト読み込み", default=True)
 	
 	def invoke(self, context, event):
 		self.filepath = context.user_preferences.addons[__name__.split('.')[0]].preferences.model_import_path
@@ -27,7 +34,10 @@ class import_cm3d2_model(bpy.types.Operator):
 		file = open(self.filepath, 'rb')
 		
 		# ヘッダー
-		ReadStr(file)
+		ext = ReadStr(file)
+		if ext != 'CM3D2_MESH':
+			self.report(type={'ERROR'}, message="これはカスタムメイド3D2のモデルファイルではありません")
+			return {'CANCELLED'}
 		struct.unpack('<i', file.read(4))[0]
 		
 		# 何で名前2つあるの？
@@ -50,11 +60,11 @@ class import_cm3d2_model(bpy.types.Operator):
 			bone_data[i]['parent_name'] = parent_name
 		for i in range(bone_count):
 			x, y, z = struct.unpack('<3f', file.read(3*4))
-			bone_data[i]['co'] = mathutils.Vector((x, y, z))
+			bone_data[i]['co'] = [x, y, z]
 			
 			w = struct.unpack('<f', file.read(4))[0]
 			x, y, z = struct.unpack('<3f', file.read(3*4))
-			bone_data[i]['rot'] = mathutils.Quaternion((x, y, z), w)
+			bone_data[i]['rot'] = [w, x, y, z]
 		
 		vertex_count, mesh_count, local_bone_count = struct.unpack('<3i', file.read(3*4))
 		
@@ -147,87 +157,151 @@ class import_cm3d2_model(bpy.types.Operator):
 			pass
 		bpy.ops.object.select_all(action='DESELECT')
 		
-		# メッシュ作成
-		me = context.blend_data.meshes.new(model_name1 + "." + model_name2)
-		verts, faces = [], []
-		for data in vertex_data:
-			verts.append(data['co'])
-		for data in face_data:
-			faces.extend(data)
-		me.from_pydata(verts, [], faces)
-		# オブジェクト化
-		ob = context.blend_data.objects.new(model_name1 + "." + model_name2, me)
-		context.scene.objects.link(ob)
-		ob.select = True
-		context.scene.objects.active = ob
-		bpy.ops.object.shade_smooth()
-		# 頂点グループ作成
-		for data in local_bone_data:
-			ob.vertex_groups.new(data['name'])
-		for vert_index, data in enumerate(vertex_data):
-			for weight in data['weights']:
-				if 0.0 < weight['value']:
-					vertex_group = ob.vertex_groups[weight['name']]
-					vertex_group.add([vert_index], weight['value'], 'REPLACE')
-		# UV作成
-		me.uv_textures.new("UVMap")
-		bm = bmesh.new()
-		bm.from_mesh(me)
-		for face in bm.faces:
-			for loop in face.loops:
-				loop[bm.loops.layers.uv.active].uv = vertex_data[loop.vert.index]['uv']
-		bm.to_mesh(me)
-		bm.free()
-		# モーフ追加
-		ob.shape_key_add(name="Basis", from_mix=False)
-		for data in misc_data:
-			if data['type'] == 'morph':
-				shape_key = ob.shape_key_add(name=data['name'], from_mix=False)
-				for vert in data['data']:
-					shape_key.data[vert['index']].co = shape_key.data[vert['index']].co + vert['co']
+		if self.is_mesh:
+			# メッシュ作成
+			me = context.blend_data.meshes.new(model_name1 + "." + model_name2)
+			verts, faces = [], []
+			for data in vertex_data:
+				verts.append(data['co'])
+			for data in face_data:
+				faces.extend(data)
+			me.from_pydata(verts, [], faces)
+			# オブジェクト化
+			ob = context.blend_data.objects.new(model_name1 + "." + model_name2, me)
+			context.scene.objects.link(ob)
+			ob.select = True
+			context.scene.objects.active = ob
+			bpy.ops.object.shade_smooth()
+			# 頂点グループ作成
+			for data in local_bone_data:
+				ob.vertex_groups.new(data['name'])
+			for vert_index, data in enumerate(vertex_data):
+				for weight in data['weights']:
+					if 0.0 < weight['value']:
+						vertex_group = ob.vertex_groups[weight['name']]
+						vertex_group.add([vert_index], weight['value'], 'REPLACE')
+			# UV作成
+			me.uv_textures.new("UVMap")
+			bm = bmesh.new()
+			bm.from_mesh(me)
+			for face in bm.faces:
+				for loop in face.loops:
+					loop[bm.loops.layers.uv.active].uv = vertex_data[loop.vert.index]['uv']
+			bm.to_mesh(me)
+			bm.free()
+			# モーフ追加
+			ob.shape_key_add(name="Basis", from_mix=False)
+			for data in misc_data:
+				if data['type'] == 'morph':
+					shape_key = ob.shape_key_add(name=data['name'], from_mix=False)
+					for vert in data['data']:
+						shape_key.data[vert['index']].co = shape_key.data[vert['index']].co + vert['co']
+			
+			# マテリアル追加
+			face_seek = 0
+			for index, data in enumerate(material_data):
+				override = context.copy()
+				override['object'] = ob
+				bpy.ops.object.material_slot_add(override)
+				name = data['name1'] + "." + data['name2'] + "." + data['name3']
+				mate = context.blend_data.materials.new(name)
+				ob.material_slots[-1].material = mate
+				# 面にマテリアル割り当て
+				for i in range(face_seek, face_seek + len(face_data[index])):
+					me.polygons[i].material_index = index
+				face_seek += len(face_data[index])
+				# テクスチャ追加
+				for tex_index, tex_data in enumerate(data['data']):
+					if tex_data['type'] == 'tex':
+						slot = mate.texture_slots.create(tex_index)
+						slot.use_map_color_diffuse = False
+						slot.color = tex_data['color'][:3]
+						slot.diffuse_color_factor = tex_data['color'][3]
+						tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
+						slot.texture = tex
+						img = context.blend_data.images.new(tex_data['name2'], 512, 512)
+						img.filepath = tex_data['path']
+						img.source = 'FILE'
+						tex.image = img
+					elif tex_data['type'] == 'col':
+						slot = mate.texture_slots.create(tex_index)
+						mate.use_textures[tex_index] = False
+						slot.use_map_color_diffuse = False
+						slot.color = tex_data['color'][:3]
+						slot.diffuse_color_factor = tex_data['color'][3]
+						slot.use_rgb_to_intensity = True
+						tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
+						slot.texture = tex
+					elif tex_data['type'] == 'f':
+						slot = mate.texture_slots.create(tex_index)
+						mate.use_textures[tex_index] = False
+						slot.use_map_color_diffuse = False
+						slot.diffuse_color_factor = tex_data['float']
+						tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
+						slot.texture = tex
+			
+			if self.is_remove_doubles:
+				bpy.ops.object.mode_set(mode='EDIT')
+				bpy.ops.mesh.select_all(action='DESELECT')
+				pre_mesh_select_mode = context.tool_settings.mesh_select_mode[:]
+				pre_mesh_select_mode = (False, True, False)
+				bpy.ops.mesh.select_non_manifold()
+				bpy.ops.mesh.remove_doubles(threshold=0.000001)
+				context.tool_settings.mesh_select_mode = pre_mesh_select_mode
+				bpy.ops.mesh.select_all(action='SELECT')
+				bpy.ops.uv.seams_from_islands()
+				bpy.ops.mesh.select_all(action='DESELECT')
+				bpy.ops.object.mode_set(mode='OBJECT')
 		
-		# マテリアル追加
-		face_seek = 0
-		for index, data in enumerate(material_data):
-			override = context.copy()
-			override['object'] = ob
-			bpy.ops.object.material_slot_add(override)
-			name = data['name1'] + "." + data['name2'] + "." + data['name3']
-			mate = context.blend_data.materials.new(name)
-			ob.material_slots[-1].material = mate
-			# 面にマテリアル割り当て
-			for i in range(face_seek, face_seek + len(face_data[index])):
-				me.polygons[i].material_index = index
-			face_seek += len(face_data[index])
-			# テクスチャ追加
-			for tex_index, tex_data in enumerate(data['data']):
-				if tex_data['type'] == 'tex':
-					slot = mate.texture_slots.create(tex_index)
-					slot.use_map_color_diffuse = False
-					slot.color = tex_data['color'][:3]
-					slot.diffuse_color_factor = tex_data['color'][3]
-					tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
-					slot.texture = tex
-					img = context.blend_data.images.new(tex_data['name2'], 512, 512)
-					img.filepath = tex_data['path']
-					img.source = 'FILE'
-					tex.image = img
-				elif tex_data['type'] == 'col':
-					slot = mate.texture_slots.create(tex_index)
-					mate.use_textures[tex_index] = False
-					slot.use_map_color_diffuse = False
-					slot.color = tex_data['color'][:3]
-					slot.diffuse_color_factor = tex_data['color'][3]
-					slot.use_rgb_to_intensity = True
-					tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
-					slot.texture = tex
-				elif tex_data['type'] == 'f':
-					slot = mate.texture_slots.create(tex_index)
-					mate.use_textures[tex_index] = False
-					slot.use_map_color_diffuse = False
-					slot.diffuse_color_factor = tex_data['float']
-					tex = context.blend_data.textures.new(tex_data['name'], 'IMAGE')
-					slot.texture = tex
+		if self.is_bone_data:
+			if "BoneData" in context.blend_data.texts.keys():
+				txt = context.blend_data.texts["BoneData"]
+				txt.clear()
+			else:
+				txt = context.blend_data.texts.new("BoneData")
+			for data in bone_data:
+				txt.write(data['name'] + ",")
+				txt.write(str(data['unknown']) + ",")
+				txt.write(str(data['parent_index']) + ",")
+				txt.write(str(data['co'][0]) + " ")
+				txt.write(str(data['co'][1]) + " ")
+				txt.write(str(data['co'][2]) + ",")
+				txt.write(str(data['rot'][0]) + " ")
+				txt.write(str(data['rot'][1]) + " ")
+				txt.write(str(data['rot'][2]) + " ")
+				txt.write(str(data['rot'][3]))
+				txt.write("\n")
+		
+		if self.is_local_bone_data:
+			if "LocalBoneData" in context.blend_data.texts.keys():
+				txt = context.blend_data.texts["LocalBoneData"]
+				txt.clear()
+			else:
+				txt = context.blend_data.texts.new("LocalBoneData")
+			for data in local_bone_data:
+				txt.write(data['name'] + ",")
+				
+				txt.write(str(data['matrix'][0][0]) + " ")
+				txt.write(str(data['matrix'][0][1]) + " ")
+				txt.write(str(data['matrix'][0][2]) + " ")
+				txt.write(str(data['matrix'][0][3]) + " ")
+				
+				txt.write(str(data['matrix'][1][0]) + " ")
+				txt.write(str(data['matrix'][1][1]) + " ")
+				txt.write(str(data['matrix'][1][2]) + " ")
+				txt.write(str(data['matrix'][1][3]) + " ")
+				
+				txt.write(str(data['matrix'][2][0]) + " ")
+				txt.write(str(data['matrix'][2][1]) + " ")
+				txt.write(str(data['matrix'][2][2]) + " ")
+				txt.write(str(data['matrix'][2][3]) + " ")
+				
+				txt.write(str(data['matrix'][3][0]) + " ")
+				txt.write(str(data['matrix'][3][1]) + " ")
+				txt.write(str(data['matrix'][3][2]) + " ")
+				txt.write(str(data['matrix'][3][3]))
+				
+				txt.write("\n")
 		
 		return {'FINISHED'}
 
