04193d227c2d80eaefecc60ce36dbe74299ee341
CM3Duser <cm3duser@gmail.com>
2015-09-17 20:17:39 +0900

    "シェイプキーの変形を拡大/縮小"のデフォルト倍率を変更

--- CM3D2 Converter/misc_tools.py
+++ CM3D2 Converter/misc_tools.py
@@ -246,7 +246,7 @@ class scale_shape_key(bpy.types.Operator):
 	bl_description = "シェイプキーの変形を強力にしたり、もしくは弱くできます"
 	bl_options = {'REGISTER', 'UNDO'}
 	
-	multi = bpy.props.FloatProperty(name="倍率", description="シェイプキーの拡大率です", default=1, min=-10, max=10, soft_min=-10, soft_max=10, step=10, precision=2)
+	multi = bpy.props.FloatProperty(name="倍率", description="シェイプキーの拡大率です", default=1.1, min=-10, max=10, soft_min=-10, soft_max=10, step=10, precision=2)
 	items = [
 		('ACTIVE', "アクティブのみ", "", 1),
 		('ALL', "全て", "", 2),
