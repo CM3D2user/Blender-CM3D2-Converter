261efb8c6f3caddd5c8da0437d392d0b9c63bf55
CM3Duser <cm3duser@gmail.com>
2015-11-23 12:39:41 +0900

    頂点数過多でmodelエクスポートを中止したときに実質頂点数を表示するように

--- CM3D2 Converter/model_export.py
+++ CM3D2 Converter/model_export.py
@@ -362,7 +362,7 @@ class export_cm3d2_model(bpy.types.Operator):
 					vert_iuv.append((vert.index, uv.x, uv.y))
 					vert_count += 1
 		if 65535 < vert_count:
-			self.report(type={'ERROR'}, message="頂点数が多過ぎます。多くても65535未満にして下さい、中止します")
+			self.report(type={'ERROR'}, message="頂点数が多過ぎます (現在" + str(vert_count) + ")。多くても65535未満にして下さい、中止します")
 			return {'CANCELLED'}
 		context.window_manager.progress_update(5)
 		
