27156ac9541c6fb41533a6ca33ee3fed8a81509a
saidenka <saidenka@yahoo.co.jp>
2017-03-01 15:13:16 +0900

    描画順の並び替え機能を改良

--- CM3D2 Converter/misc_VIEW3D_MT_edit_mesh_specials.py
+++ CM3D2 Converter/misc_VIEW3D_MT_edit_mesh_specials.py
@@ -5,13 +5,13 @@ from . import common
 def menu_func(self, context):
 	icon_id = common.preview_collections['main']['KISS'].icon_id
 	self.layout.separator()
-	self.layout.operator('mesh.selected_mesh_sort_front', text="選択メッシュの描画順を最前面に", icon_value=icon_id).is_back = False
-	self.layout.operator('mesh.selected_mesh_sort_front', text="選択メッシュの描画順を最背面に", icon_value=icon_id).is_back = True
+	self.layout.operator('mesh.selected_face_sort_front', text="選択面の描画順を最前面に", icon_value=icon_id).is_back = False
+	self.layout.operator('mesh.selected_face_sort_front', text="選択面の描画順を最背面に", icon_value=icon_id).is_back = True
 
 class selected_mesh_sort_front(bpy.types.Operator):
-	bl_idname = 'mesh.selected_mesh_sort_front'
-	bl_label = "選択メッシュの描画順を最前面に"
-	bl_description = "選択中のメッシュの描画順を最も前面にソートします"
+	bl_idname = 'mesh.selected_face_sort_front'
+	bl_label = "選択面の描画順を最前面に"
+	bl_description = "選択中の面の描画順を最も前面/背面に並び替えます"
 	bl_options = {'REGISTER', 'UNDO'}
 	
 	is_back = bpy.props.BoolProperty(name="最背面")
@@ -23,37 +23,32 @@ class selected_mesh_sort_front(bpy.types.Operator):
 		return True
 	
 	def execute(self, context):
-		pre_obs = []
-		for ob in context.selectable_objects:
-			pre_obs.append(ob.name)
+		ob = context.active_object
+		if ob.mode != 'EDIT':
+			self.report(type={'ERROR'}, message="エディットモードで実行してください")
+			return {'CANCELLED'}
+		me = ob.data
+		bm = bmesh.from_edit_mesh(me)
 		
-		source_ob = context.active_object
-		pre_ob_name = source_ob.name
-		pre_me_name = source_ob.data.name
+		bm.faces.ensure_lookup_table()
 		
-		bpy.ops.mesh.separate(type='SELECTED')
-		bpy.ops.object.mode_set(mode='OBJECT')
-		bpy.ops.object.select_all(action='DESELECT')
+		selected_face_indexs = []
+		other_face_indexs = []
+		for face in bm.faces:
+			if face.select:
+				selected_face_indexs.append(face.index)
+			else:
+				other_face_indexs.append(face.index)
 		
-		source_ob.select = True
-		for ob in context.selectable_objects:
-			if (not ob.name in pre_obs):
-				ob.select = True
-				new_ob = ob
-				break
-		for vert in new_ob.data.vertices:
-			vert.select = True
-		
+		output_face_indexs = []
 		if not self.is_back:
-			context.scene.objects.active = source_ob
+			output_face_indexs = other_face_indexs + selected_face_indexs
 		else:
-			context.scene.objects.active = new_ob
+			output_face_indexs = selected_face_indexs + other_face_indexs
 		
-		bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
-		bpy.ops.object.join()
+		for for_index, sorted_index in enumerate(output_face_indexs):
+			bm.faces[sorted_index].index = for_index
 		
-		context.scene.objects.active.name = pre_ob_name
-		context.scene.objects.active.data.name = pre_me_name
-		
-		bpy.ops.object.mode_set(mode='EDIT')
+		bm.faces.sort()
+		bmesh.update_edit_mesh(me)
 		return {'FINISHED'}
