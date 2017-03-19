093a1400906909752c399384ffa07e9363607943
saidenka <saidenka@yahoo.co.jp>
2017-03-19 15:11:30 +0900

    CM3D2用の法線計算処理をより正確に

--- CM3D2 Converter/misc_DATA_PT_modifiers.py
+++ CM3D2 Converter/misc_DATA_PT_modifiers.py
@@ -1,4 +1,4 @@
-import os, re, sys, bpy, time, bmesh, mathutils
+import os, re, sys, bpy, time, bmesh, mathutils, math
 from . import common
 
 # メニュー等に項目追加
@@ -62,11 +62,15 @@ class forced_modifier_apply(bpy.types.Operator):
 			arm = arm_ob.data
 			arm_pose = arm_ob.pose
 			
-			pose_rots = {}
+			pose_quats = {}
 			for bone in arm.bones:
-				base_quat = bone.matrix_local.to_quaternion()
-				pose_quat = arm_pose.bones[bone.name].matrix.to_quaternion()
-				pose_rots[bone.name] = base_quat.rotation_difference(pose_quat)
+				pose_bone = arm_pose.bones[bone.name]
+				
+				bone_quat = bone.matrix_local.to_quaternion()
+				pose_quat = pose_bone.matrix.to_quaternion()
+				result_quat = pose_quat * bone_quat.inverted()
+				
+				pose_quats[bone.name] = result_quat.copy()
 			
 			custom_normals = []
 			for loop in me.loops:
@@ -77,51 +81,53 @@ class forced_modifier_apply(bpy.types.Operator):
 				for vge in vert.groups:
 					total_weight += vge.weight
 				
-				total_rot = mathutils.Quaternion()
+				total_quat = mathutils.Quaternion()
 				for vge in vert.groups:
 					vg = ob.vertex_groups[vge.group]
-					total_rot = total_rot.slerp(pose_rots[vg.name], vge.weight / total_weight)
+					total_quat = total_quat.slerp(pose_quats[vg.name], vge.weight / total_weight)
 				
-				no.rotate(total_rot)
+				no.rotate(total_quat)
 				custom_normals.append(no)
 		
-		pre_relative_keys = [s.relative_key.name for s in me.shape_keys.key_blocks]
-		pre_active_shape_key_index = ob.active_shape_key_index
 		pre_selected_objects = context.selected_objects[:]
 		pre_mode = ob.mode
 		
-		shape_names = [s.name for s in me.shape_keys.key_blocks]
-		shape_deforms = []
-		for shape in me.shape_keys.key_blocks:
-			shape_deforms.append([shape.data[v.index].co.copy() for v in me.vertices])
-		
-		ob.active_shape_key_index = len(me.shape_keys.key_blocks) - 1
-		for i in me.shape_keys.key_blocks[:]:
-			ob.shape_key_remove(ob.active_shape_key)
-		
-		new_shape_deforms = []
-		for shape_index, deforms in enumerate(shape_deforms):
+		if me.shape_keys:
+			pre_relative_keys = [s.relative_key.name for s in me.shape_keys.key_blocks]
+			pre_active_shape_key_index = ob.active_shape_key_index
 			
-			temp_ob = ob.copy()
-			temp_me = me.copy()
-			temp_ob.data = temp_me
-			context.scene.objects.link(temp_ob)
+			shape_names = [s.name for s in me.shape_keys.key_blocks]
+			shape_deforms = []
+			for shape in me.shape_keys.key_blocks:
+				shape_deforms.append([shape.data[v.index].co.copy() for v in me.vertices])
 			
-			for vert in temp_me.vertices:
-				vert.co = deforms[vert.index].copy()
+			ob.active_shape_key_index = len(me.shape_keys.key_blocks) - 1
+			for i in me.shape_keys.key_blocks[:]:
+				ob.shape_key_remove(ob.active_shape_key)
 			
-			override = context.copy()
-			override['object'] = temp_ob
-			for index, mod in enumerate(temp_ob.modifiers):
-				if self.is_applies[index]:
-					try:
-						bpy.ops.object.modifier_apply(override, modifier=mod.name)
-					except: pass
-			
-			new_shape_deforms.append([v.co.copy() for v in temp_me.vertices])
-			
-			common.remove_data(temp_ob)
-			common.remove_data(temp_me)
+			new_shape_deforms = []
+			for shape_index, deforms in enumerate(shape_deforms):
+				
+				temp_ob = ob.copy()
+				temp_me = me.copy()
+				temp_ob.data = temp_me
+				context.scene.objects.link(temp_ob)
+				
+				for vert in temp_me.vertices:
+					vert.co = deforms[vert.index].copy()
+				
+				override = context.copy()
+				override['object'] = temp_ob
+				for index, mod in enumerate(temp_ob.modifiers):
+					if self.is_applies[index]:
+						try:
+							bpy.ops.object.modifier_apply(override, modifier=mod.name)
+						except: pass
+				
+				new_shape_deforms.append([v.co.copy() for v in temp_me.vertices])
+				
+				common.remove_data(temp_ob)
+				common.remove_data(temp_me)
 		
 		for index, mod in enumerate(ob.modifiers[:]):
 			if self.is_applies[index]:
@@ -131,21 +137,24 @@ class forced_modifier_apply(bpy.types.Operator):
 					ob.modifiers.remove(mod)
 		
 		context.scene.objects.active = ob
-		for shape_index, deforms in enumerate(new_shape_deforms):
-			
-			bpy.ops.object.shape_key_add(from_mix=False)
-			shape = ob.active_shape_key
-			shape.name = shape_names[shape_index]
-			
-			for vert in me.vertices:
-				shape.data[vert.index].co = deforms[vert.index].copy()
 		
-		for shape_index, shape in enumerate(me.shape_keys.key_blocks):
-			shape.relative_key = me.shape_keys.key_blocks[pre_relative_keys[shape_index]]
+		if me.shape_keys:
+			for shape_index, deforms in enumerate(new_shape_deforms):
+				
+				bpy.ops.object.shape_key_add(from_mix=False)
+				shape = ob.active_shape_key
+				shape.name = shape_names[shape_index]
+				
+				for vert in me.vertices:
+					shape.data[vert.index].co = deforms[vert.index].copy()
+			
+			for shape_index, shape in enumerate(me.shape_keys.key_blocks):
+				shape.relative_key = me.shape_keys.key_blocks[pre_relative_keys[shape_index]]
+			
+			ob.active_shape_key_index = pre_active_shape_key_index
 		
-		ob.active_shape_key_index = pre_active_shape_key_index
-		for o in pre_selected_objects:
-			o.select = True
+		for temp_ob in pre_selected_objects:
+			temp_ob.select = True
 		bpy.ops.object.mode_set(mode=pre_mode)
 		
 		if arm_ob:
