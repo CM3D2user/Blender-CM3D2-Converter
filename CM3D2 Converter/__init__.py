999b8b5be8af947eb68cf43380fd503e8c720c8c
saidenka <saidenka@yahoo.co.jp>
2017-03-25 18:36:30 +0900

    anmエクスポート機能の改良2

--- CM3D2 Converter/__init__.py
+++ CM3D2 Converter/__init__.py
@@ -4,7 +4,7 @@
 bl_info = {
 	"name" : "CM3D2 Converter",
 	"author" : "",
-	"version" : (0, 410),
+	"version" : (0, 411),
 	"blender" : (2, 7),
 	"location" : "ファイル > インポート/エクスポート > CM3D2 Model (.model)",
 	"description" : "カスタムメイド3D2の専用ファイルのインポート/エクスポートを行います",
