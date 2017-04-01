be5da350a1c09f526077ddfc708e0d65e5caa857
saidenka <saidenka@yahoo.co.jp>
2017-04-01 14:39:37 +0900

    anm出力時の変形の補完を改良

--- CM3D2 Converter/__init__.py
+++ CM3D2 Converter/__init__.py
@@ -4,7 +4,6 @@
 bl_info = {
 	"name" : "CM3D2 Converter",
 	"author" : "",
-	"version" : (0, 429),
 	"blender" : (2, 76, 0),
 	"location" : "ファイル > インポート/エクスポート > CM3D2 Model (.model)",
 	"description" : "カスタムメイド3D2の専用ファイルのインポート/エクスポートを行います",
