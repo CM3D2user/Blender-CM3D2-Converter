8c5284abefaef33cc4a251a1b36ce134299b921f
saidenka <saidenka@yahoo.co.jp>
2017-04-04 17:21:42 +0900

    KISSアイコンの視認性の向上

--- CM3D2 Converter/__init__.py
+++ CM3D2 Converter/__init__.py
@@ -4,7 +4,7 @@
 bl_info = {
 	"name" : "CM3D2 Converter",
 	"author" : "",
-	"version" : (20170404, 991132),
+	"version" : (20170404, 991721),
 	"blender" : (2, 76, 0),
 	"location" : "ファイル > インポート/エクスポート > CM3D2 Model (.model)",
 	"description" : "カスタムメイド3D2の専用ファイルのインポート/エクスポートを行います",
