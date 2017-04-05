5d757e6f482ac4fae3eec685ebb4638d79c9a95c
saidenka <saidenka@yahoo.co.jp>
2017-04-05 16:04:55 +0900

    更新履歴表示機能を改良

--- CM3D2 Converter/misc_INFO_MT_help.py
+++ CM3D2 Converter/misc_INFO_MT_help.py
@@ -22,29 +22,31 @@ class INFO_MT_help_CM3D2_Converter_RSS(bpy.types.Menu):
 			titles = re.findall(r'\<title\>[　\s]*([^　\s][^\<]*[^　\s])[　\s]*\<\/title\>', html)[1:]
 			updates = re.findall(r'\<updated\>([^\<\>]*)\<\/updated\>', html)[1:]
 			links = re.findall(r'<link [^\<\>]*href="([^"]+)"/>', html)[2:]
-			count = 0
 			version_datetime = datetime.datetime.strptime(str(common.bl_info["version"][0]) + str(common.bl_info["version"][1]), '%Y%m%d99%H%M')
+			
+			output_data = []
+			update_diffs = []
 			for title, update, link in zip(titles, updates, links):
 				title = xml.sax.saxutils.unescape(title, {'&quot;': '"'})
 				
 				rss_datetime = datetime.datetime.strptime(update, '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(hours=9)
 				diff_seconds = datetime.datetime.now() - rss_datetime
 				icon = 'SORTTIME'
-				if 7 < diff_seconds.days:
+				if 60 * 60 * 24 * 7 < diff_seconds.total_seconds():
 					icon = 'NLA'
-				elif 3 < diff_seconds.days:
+				elif 60 * 60 * 24 * 3 < diff_seconds.total_seconds():
 					icon = 'COLLAPSEMENU'
-				elif 1 <= diff_seconds.days:
+				elif 60 * 60 * 24 < diff_seconds.total_seconds():
 					icon = 'TIME'
-				elif diff_seconds.days == 0 and 60 * 60 < diff_seconds.total_seconds():
+				elif 60 * 60 < diff_seconds.total_seconds():
 					icon = 'RECOVER_LAST'
-				elif diff_seconds.total_seconds() <= 60 * 60:
+				else:
 					icon = 'PREVIEW_RANGE'
 				
-				if diff_seconds.days:
-					date_str = "%d日前" % diff_seconds.days
+				if 60 * 60 * 24 <= diff_seconds.total_seconds():
+					date_str = "%d日前" % int(diff_seconds.total_seconds() / 60 / 60 / 24)
 				elif 60 * 60 <= diff_seconds.total_seconds():
-					date_str = "%d時間前" % int(diff_seconds.total_seconds() / (60 * 60))
+					date_str = "%d時間前" % int(diff_seconds.total_seconds() / 60 / 60)
 				elif 60 <= diff_seconds.total_seconds():
 					date_str = "%d分前" % int(diff_seconds.total_seconds() / 60)
 				else:
@@ -52,12 +54,19 @@ class INFO_MT_help_CM3D2_Converter_RSS(bpy.types.Menu):
 				
 				text = "(" + date_str + ") " + title
 				
-				if (version_datetime - rss_datetime).total_seconds() / 60 / 60 < 1:
+				update_diff = abs( (version_datetime - rss_datetime).total_seconds() )
+				
+				output_data.append((text, icon, link, update_diff))
+				update_diffs.append(update_diff)
+			
+			min_update_diff = sorted(update_diffs)[0]
+			for text, icon, link, update_diff in output_data:
+				
+				if update_diff == min_update_diff:
 					text = "Now! " + text
 					icon = 'QUESTION'
 				
 				self.layout.operator('wm.url_open', text=text, icon=icon).url = link
-				count += 1
 		except:
 			self.layout.label(text="更新の取得に失敗しました", icon='ERROR')
 
